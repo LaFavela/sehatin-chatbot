a6061cf89bc929d4516d118793b917ee64d2182a
DzulhiRaihan <dzulhiraihan@gmail.com>
2025-05-09 00:32:11 +0800

    add Ollama import

--- recomendation.py
+++ recomendation.py
@@ -1,4 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
 from calculate import calculate_bmi, calculate_bmr, calculate_tdee
 from retriever import food_retriever
 import time
